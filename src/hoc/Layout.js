397f8b62567764ce6a53d7d874c45d4beb7fa6a4
Luis Guillermo <luisg.trevinoc@gmail.com>
2021-04-22 15:12:00 -0500

    Add layout

---
+++ src/hoc/Layout.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+
+class Layout extends React.Component{
+    render(){
+        return(
+            <Fragment>
+                <div>HEADER</div>
+                <div>MAIN</div>
+            </Fragment>
+        );
+    }
+}
+
+export default Layout;
